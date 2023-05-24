462dedba640bc1e060f69bb7c591ec3d8e3adf21
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-05-24 08:29:13 +0800

    delete makefile

---
+++ test/SmartPtr/SmartPtr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <loki/ThreadModel.h>
+#include <mutex>
+#include <thread>
+
+
+using namespace Loki;
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+}
